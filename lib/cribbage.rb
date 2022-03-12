6364be410057256e5f31165a03f6105238ebef53
Aaron Webster <aaron.l.webster@gmail.com>
2022-03-12 09:10:19 +0100

    First commit

---
+++ lib/cribbage.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cribbage/version"
+
+module Cribbage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cribbage_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cribbage do
+  it "has a version number" do
+    expect(Cribbage::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
