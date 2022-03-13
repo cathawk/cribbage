7e2c6691a59ac830136df4ba0b32de7a3fc95777
Aaron Webster <aaron.l.webster@gmail.com>
2022-03-13 10:09:40 +0100

    Creates start holes

--- lib/cribbage/board_components/street.rb
+++ lib/cribbage/board_components/street.rb
@@ -3,12 +3,20 @@
 module Cribbage
   module BoardComponents
     class Street
-      NUMBER_HOLES = 120
+      NUMBER_HOLES = 120.freeze
+      NUMBER_START_HOLES = 2.freeze
 
-      attr_reader :holes
+      attr_reader :holes, :start_holes
 
       def initialize
-        @holes = (0..NUMBER_HOLES).each_with_index.map { |hole_number|
+        @holes = setup_holes(NUMBER_HOLES)
+        @start_holes = setup_holes(NUMBER_START_HOLES)
+      end
+
+      private
+
+      def setup_holes(number_holes)
+        (0..number_holes).each_with_index.map { |hole_number|
           Hole.new(street: self, index: hole_number, contains_peg: false)
         }
       end
